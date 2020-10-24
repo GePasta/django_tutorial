e8bcbe987e98beb9ca123bb6007ccad5c3d4314c
Adam Osipiuk <adam.osipiuk@aviolinx.com>
2020-10-24 12:02:17 +0200

    Add bmi weight

--- main.py
+++ main.py
@@ -8,9 +8,25 @@ def get_bmi(weight, height):
     return weight / height ** 2
 
 
+def bmi_status(bmi):
+    """
+
+    :param bmi: bmi ratio
+    :return: status of your condition
+    """
+    if bmi < 18.5:
+        return "Niedowaga"
+    elif 18.5 <= bmi and bmi >= 25:
+        return "Norma"
+    elif 25 > bmi and bmi <= 30:
+        return "Nadwaga"
+    elif bmi > 30:
+        return "Otylosc"
+
+
 if __name__ == "__main__":
     weight = input("Input weight in kilograms:")
     height = input("Input height in meters")
     height = height.replace(",", ".")
     bmi = get_bmi(float(weight), float(height))
-    print(f"Your BMI is {bmi}")
+    print(f"Your BMI is {bmi} and your weight status is {bmi_status(bmi)}")
