61eb6181d7687dcfdebd5c1e0d5223cc28f80581
Tomasz Pietrzak <Tomasz.Pietrzak@lppsa.com>
2020-10-24 15:57:59 +0200

    Views

---
+++ dj/myapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime, timedelta
+
+def view_1(request):
+    return HttpResponse("view_1 content")
+
+
+def default(request):
+    return HttpResponse("domyslny komunikat")
+
+
+def about(request):
+    return HttpResponse("Adam i Tomek")
+
+
+def death_time(request):
+    result = datetime.now() + timedelta(days=10)
+    return HttpResponse(f"Umrzemy - {result}")
