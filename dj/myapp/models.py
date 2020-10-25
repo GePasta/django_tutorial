6b43409e99bc5a69921f908e3b41b3ec9f0ed191
GePasta <adam.osipiuk@aviolinx.com>
2020-10-25 11:09:20 +0100

    Add sex types

--- dj/myapp/models.py
+++ dj/myapp/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 
 class Person(models.Model):
+    SEX_TYPES = (
+        ('M', 'Male'),
+        ('F', "Female"),
+    )
     age = models.IntegerField()
-    height = models.DecimalField(max_digits=10, decimal_places=2)
-    name = models.CharField(max_length=50)
+    height = models.IntegerField()
+    name = models.CharField(max_length=100)
+    mass = models.IntegerField(blank=True, null=True)
+    sex = models.CharField(max_length=10, null=True, choices=SEX_TYPES)
     email = models.EmailField(blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
 
