1916443c7bc73b98a02beba0690456c7d0fc80a6
Tomasz Pietrzak <Tomasz.Pietrzak@lppsa.com>
2020-10-25 10:49:33 +0100

    sqlite

--- dj/myapp/models.py
+++ dj/myapp/models.py
@@ -5,3 +5,8 @@ class Person(models.Model):
     age = models.IntegerField()
     height = models.DecimalField(max_digits=10, decimal_places=2)
     name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Name: {self.name}, age: {self.age}"
